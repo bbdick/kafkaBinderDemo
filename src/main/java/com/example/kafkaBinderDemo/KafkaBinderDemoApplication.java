41a34ae18d9d5bb036ade1bbacd211925e6eb6df
Barbara Dick <c-barbara.dick@charter.com>
2021-03-04 14:46:02 -0700

    workaround in api-service-commons

--- src/main/java/com/example/kafkaBinderDemo/KafkaBinderDemoApplication.java
+++ src/main/java/com/example/kafkaBinderDemo/KafkaBinderDemoApplication.java
@@ -1,17 +1,18 @@
 package com.example.kafkaBinderDemo;
 
+import com.charter.scp.scl.api.commons.ApiServiceBase;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableBinding({MyOutputSource.class, MyInputSource.class})
-@SpringBootApplication
+//@Import(SwaggerConfig.class)
+//@SpringBootApplication
 @Slf4j
-public class KafkaBinderDemoApplication {
+public class KafkaBinderDemoApplication extends ApiServiceBase {
 	private static ConfigurableApplicationContext ctx;
 
 	public static void main(String[] args) {
